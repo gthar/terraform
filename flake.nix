ce97725c4d5b297221101eec722117bc76f67897
Ricard Illa <rilla@monotremata.xyz>
2022-08-18 18:10:19 +0200

    nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "shell for my terraform things";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.bashInteractive pkgs.terraform ];
+          buildInputs = [ ];
+        };
+      });
+}
